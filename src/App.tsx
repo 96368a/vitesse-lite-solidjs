726a94a37659471dccbf745bd6ed2d41fbbfe7ba
96368a <ncxxg@qq.com>
2024-01-30 08:38:09 +0800

    :bug: 修复路由问题

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import Footer from './components/Footer'
 import routes from '~solid-pages'
 
 export default function App() {
+  const Routes = useRoutes(routes)
   return (
     <main class="font-sans px-4 py-10 text-center text-gray-700 dark:text-gray-200">
       <Router>
-        {routes}
+        {Routes()}
       </Router>
       <Footer />
     </main>
